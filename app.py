e73c8651e0ed2db4d4605abed6fc57c98eea65c3
Bhugan <129927643+Bhugan@users.noreply.github.com>
2024-01-22 20:47:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -19,36 +19,35 @@ def extract_text_from_pdf(file_path):
         text = page.extract_text()
     return text
 
-choice = st.sidebar.selectbox("Select your choice", ["Summarize Text", "Summarize Document"])
+# Initial choice to analyze single text or document
+choice = st.sidebar.radio("Select your choice", ["Analyze Single Text", "Analyze Document"])
 
-if choice == "Summarize Text":
-    st.subheader("Summarize Text using Hugging Face Transformers")
+if choice == "Analyze Single Text":
+    st.subheader("Analyze Single Text using Hugging Face Transformers")
     input_text = st.text_area("Enter your text here")
-    if input_text is not None:
-        if st.button("Summarize Text"):
-            col1, col2 = st.columns([1, 1])
-            with col1:
-                st.markdown("**Your Input Text**")
-                st.info(input_text)
-            with col2:
-                st.markdown("**Summary Result**")
-                result = text_summary(input_text)
-                st.success(result)
+    if input_text is not None and st.button("Analyze"):
+        col1, col2 = st.columns([1, 1])
+        with col1:
+            st.markdown("**Your Input Text**")
+            st.info(input_text)
+        with col2:
+            st.markdown("**Analysis Result**")
+            result = text_summary(input_text)
+            st.success(result)
 
-elif choice == "Summarize Document":
-    st.subheader("Summarize Document using Hugging Face Transformers")
+elif choice == "Analyze Document":
+    st.subheader("Analyze Document using Hugging Face Transformers")
     input_file = st.file_uploader("Upload your document here", type=['pdf'])
-    if input_file is not None:
-        if st.button("Summarize Document"):
-            with open("doc_file.pdf", "wb") as f:
-                f.write(input_file.getbuffer())
-            col1, col2 = st.columns([1, 1])
-            with col1:
-                st.info("File uploaded successfully")
-                extracted_text = extract_text_from_pdf("doc_file.pdf")
-                st.markdown("**Extracted Text is Below:**")
-                st.info(extracted_text)
-            with col2:
-                st.markdown("**Summary Result**")
-                doc_summary = text_summary(extracted_text)
-                st.success(doc_summary)
+    if input_file is not None and st.button("Analyze"):
+        with open("doc_file.pdf", "wb") as f:
+            f.write(input_file.getbuffer())
+        col1, col2 = st.columns([1, 1])
+        with col1:
+            st.info("File uploaded successfully")
+            extracted_text = extract_text_from_pdf("doc_file.pdf")
+            st.markdown("**Extracted Text is Below:**")
+            st.info(extracted_text)
+        with col2:
+            st.markdown("**Analysis Result**")
+            doc_summary = text_summary(extracted_text)
+            st.success(doc_summary)
